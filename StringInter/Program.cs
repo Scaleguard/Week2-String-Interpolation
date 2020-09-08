5132d95f7d21bceb9e7cbde0bc1e2f48c8caf3c7
Opilane TTHK <opilane@tthk.ee>
2020-09-08 12:04:28 +0300

    Commit My String Interpolation App

---
+++ StringInter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. The app asks for the user's first name and stores it in a separate variable
+             * 2. The app asks for the user's last name and stores it in a separate variable
+             */
+
+            Console.WriteLine("What's your first name?");
+            string FirstName;
+            string LastName;
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+            //Console.WriteLine("Hello there, " + FirstName + " " + LastName);
+            //string interpolation
+            //$ - alt + ctrl + 4 { - alt gr + 7, } - alt gr + 0
+            Console.WriteLine($"Hello there, {FirstName} {LastName}. ");
+
+
+
+
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. Program asks for the users first- and lastname
+             * 2. Program asks for the users year of birth
+             * 3. Program greets the user by ther name and displays their age.
+             NB! To display the result use one WrtieLine only!
+             */
+            Console.WriteLine("What is your first name?");
+            string FirstName;
+            string LastName;
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("What year were you born in?");
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int YearOfBirth;
+            YearOfBirth = Int32.Parse(UserInput);
+            int Age = 2020 - YearOfBirth;
+
+            Console.WriteLine($"Hello there, {FirstName} {LastName}, you're also {Age} years old.  ");
+
+
+
+
+        }
+    }
+}
